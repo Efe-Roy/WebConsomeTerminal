02a2e92ff34a5d9526d5b5cee23e584068b90080
Efe-Roy <dakaraefe2@gmail.com>
2023-01-17 12:04:22 +0100

    first commit

---
+++ src/components/commands/About.tsx
@@ -0,0 +1,31 @@
+import { Command } from "../../styles/common";
+const About = () => {
+  return (
+    <>
+      <p data-testid="About">
+        <strong>Hi!</strong> My name is Dakara Efe Roy
+      </p>
+      <p>
+        I'm a <strong>Software developer</strong> currently working from home
+      </p>
+      <p>
+        I'm taking my major in <strong>Software Engeneering</strong> and
+        studying/working very hard on my software development skills in order to
+        become a better developer
+      </p>
+      <p>
+        If you liked this project and would like to take a look at the source
+        code or contact me, use the <Command>source</Command>,
+        <Command>social</Command> or
+        <Command>connect "socialMedia"</Command> commands
+      </p>
+      <p>
+        In case you have any doubts on the commands, type
+        <Command>help</Command> to display a list of commands and their
+        description
+      </p>
+    </>
+  );
+};
+
+export default About;
---
+++ src/styles/common.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Command = styled.code`
+    background-color: #576F72;
+    font-weight: bold;
+    padding: 0.2em;
+    border-radius: 5px;
+    margin-left: 5px;
+    
+`
